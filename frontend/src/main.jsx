c2bccb151a28813b1beb9080b8342ed30fc6a078
Alonso <alonso.vera@mail.udp.cl>
2025-10-04 13:12:20 -0300

    reinicio de todo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-// 1. Importa 'extendTheme' además de ChakraProvider
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-const theme = extendTheme({});
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* 3. Pásale el tema que acabamos de crear al Provider */}
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
