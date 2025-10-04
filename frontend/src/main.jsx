89270b4bdcddd1af444211a7ebd347da649cd97d
Alonso <alonso.vera@mail.udp.cl>
2025-10-04 12:20:56 -0300

    Solución de errores con el Chakra

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// 1. Importa 'extendTheme' además de ChakraProvider
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const theme = extendTheme({});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 3. Pásale el tema que acabamos de crear al Provider */}
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
